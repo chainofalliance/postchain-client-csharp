3d2f4b0846b890ff87de47a9e97feb4c9afcfc5f
Dennis Kirsch <dennis.kirsch@haw-hamburg.de>
2019-08-16 14:25:38 +0200

    Implement some functions in restclient

--- src/Restclient.cs
+++ src/Restclient.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace Chromia.PostchainClient
 {
@@ -14,20 +15,55 @@ namespace Chromia.PostchainClient
             this._maxSockets = maxSockets;
         }
 
-        public void getTransaction(string messageHash, Action<string, int, string> callback){
-            throw new NotImplementedException("Please create a test first.");
+        public void getTransaction(string messageHash, Action<string, dynamic> callback){
+            _validateMessageHash(messageHash);
+            Action<string,int,dynamic> cb = delegate(string error, int statusCode, dynamic responseObject){
+                _handleGetResponse(error, statusCode, statusCode == 200 ? StringToHex(responseObject["tx"].ToString()) : null, callback);
+            };
+
+            _get(this._urlBase, "tx/" + this._blockhainRID + "/" + StringToHex(messageHash), cb);
         }
 
-        public void postTransaction(string serializedTransaction, Action<string, int, string> callback){
-            throw new NotImplementedException("Please create a test first.");
+        public void postTransaction(string serializedTransaction, Action<string, dynamic> callback){
+            string jsonString = @"{tx: " + StringToHex(serializedTransaction) + "}";
+            var jsonObject = Newtonsoft.Json.JsonConvert.DeserializeObject<dynamic>(jsonString);
+
+            _doPost(this._urlBase, "tx/" + this._blockhainRID, jsonObject, callback);
         }
 
         public void getConfirmationProof(string messageHash, Action<string, string> callback){
-            throw new NotImplementedException("Please create a test first.");
+            _validateMessageHash(messageHash);
+            Action<string,int,dynamic> cb = delegate(string error, int statusCode, dynamic responseObject){
+                if (statusCode == 200){
+                    responseObject["hash"] = _b(responseObject["hash"].ToString());
+                    responseObject["blockHeader"] = _b(responseObject["blockHeader"].ToString());
+                    if (responseObject["signatures"].ToString() != ""){
+                        for (int i = 0; i < responseObject["signatures"].Count; i++)
+                        {
+                            responseObject["signatures"][i]["pubKey"] = _b(responseObject["signatures"][i]["pubKey"].ToString());
+                            responseObject["signatures"][i]["signature"] = _b(responseObject["signatures"][i]["signature"].ToString());
+                        }
+                    }
+
+                    if (responseObject["merklePath"].ToString() != ""){
+                        for (int i = 0; i < responseObject["merklePath"].Count; i++)
+                        {
+                            responseObject["merklePath"][i]["hash"] = _b(responseObject["merklePath"][i]["hash"].ToString());
+                        }
+                    }
+                }
+            };
+
+            _get(_urlBase, "tx/" + this._blockhainRID + "/" + StringToHex(messageHash) + "/confirmationProof", cb);
         }
 
-        public void status(string messageHash, Action<string, string> callback){
-            throw new NotImplementedException("Please create a test first.");
+        public void status(string messageHash, Action<string, dynamic> callback){
+            _validateMessageHash(messageHash);
+            Action<string,int,dynamic> cb = delegate(string error, int statusCode, dynamic responseObject){
+                _handleGetResponse(error, statusCode, responseObject, callback);
+            };
+
+            _get(this._urlBase, "tx/" + this._blockhainRID + "/" + StringToHex(messageHash) + "/status", cb);
         }
 
         public void query(string queryName, string queryObject){
@@ -42,11 +78,11 @@ namespace Chromia.PostchainClient
             throw new NotImplementedException("Please create a test first.");
         }
 
-        private void _doPost(string config, string path, string jsonObject, Action<string, string> responseCallback){
+        private void _doPost(string config, string path, string jsonObject, Action<string, dynamic> responseCallback){
             throw new NotImplementedException("Please create a test first.");
         }
 
-        private void _get(string config, string path, Action<string, int, string> callback){
+        private void _get(string config, string path, Action<string, int, dynamic> callback){
             throw new NotImplementedException("Please create a test first.");
         }
 
@@ -58,11 +94,20 @@ namespace Chromia.PostchainClient
             throw new NotImplementedException("Please create a test first.");
         }
 
-        private void _handleGetResponse(bool error, int statusCode, string responseObject, Action<string, string> callback){
+        private void _handleGetResponse(string error, int statusCode, string responseObject, Action<string, dynamic> callback){
             throw new NotImplementedException("Please create a test first.");
         }
 
-        private void _b(string stringValue){
+        private string StringToHex(string stringValue)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (char c in stringValue)
+            { 
+                sb.Append(Convert.ToInt32(c).ToString("X")); 
+            }
+            return sb.ToString();
+        }
+        private string _b(string stringValue){
             throw new NotImplementedException("Please create a test first.");
         }
     }
