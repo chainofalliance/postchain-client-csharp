4fec3ed8361dca9b5d7e151b0f0f51294aa29b0d
Simon <simon.sorgenfrei@haw-hamburg.de>
2019-08-21 12:55:35 +0200

    added util keypair test

--- test/UtilTest.cs
+++ test/UtilTest.cs
@@ -1,9 +1,26 @@
 using Xunit;
+using System;
 
 namespace Chromia.PostchainClient.Tests.GTX
 {
     public class UtilTest
     {
+        [Fact]
+        public void CheckKeyPairLength(){
+            const int expPrivKeyLength = 32;
+            const int expPubKeyLength = 33;
 
+            var user = Util.MakeKeyPair();
+            var privKey = user["privKey"];
+            var pubKey = user["pubKey"];
+
+            Assert.Equal(privKey.Length, expPrivKeyLength);
+            Assert.Equal(pubKey.Length, expPubKeyLength);
+        }
+
+        [Fact]
+        public void CheckKeyPairAuthenticity(){           
+            //const privKey = "0101010101010101010101010101010101010101010101010101010101010101";
+        }
     }
 }
